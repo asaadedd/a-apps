5412fb3acaefd2d6ec8ec0c02d534908ffbd4184
asaadedd <saadeddineadnan@gmail.com>
2019-07-20 19:28:02 +0300

    add auth graphql

--- apps/e-appointment-serve/src/app/auth/schemas/user.schema.ts
+++ apps/e-appointment-serve/src/app/auth/schemas/user.schema.ts
@@ -1,11 +1,9 @@
 import { Schema } from 'mongoose';
 
 export const userSchema = new Schema({
-  name: String,
-  age: Number,
-  breed: String,
-  numberOfAppointments: Number,
-  password: String,
   username: String,
+  firstName: String,
+  lastName: String,
   email: String,
+  dateOfBirth: String
 });
